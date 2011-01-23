ff84a5bbf064180e26fb289a507e2afb18831322
Antonio <tonyx@yahoo.com>
2011-01-23 02:45:06 +0100

    Merge branch 'master' of github.com:tonyx/Translate
    Conflicts:
    	src/com/tonyxzt/language/GoogleDictionary.java
    	src/com/tonyxzt/language/Translator.java

--- src/refactoring/com/tonyxzt/language/RefactoredCommandLineToStatusClassWrapper.java
+++ src/refactoring/com/tonyxzt/language/RefactoredCommandLineToStatusClassWrapper.java
@@ -14,10 +14,17 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 public class RefactoredCommandLineToStatusClassWrapper {
-
     public void setStatusReadyForTheAction(RefactoredTranslator translator, String[] strIn,Map<String,GenericDictionary> dics)  {
-        boolean inlineread=true;
-        boolean stdOut = true;
+        translator.setInputStream(new SimpleInputStream(new String[] {strIn[strIn.length-1]}));
+
+        translator.setOutStream(new OutStream(){
+            public void output(String out) {
+                System.out.print(out);
+            }
+            public void close() {
+            }
+        });
+
         for (String aStrIn : strIn) {
             if (aStrIn.startsWith("--dic")) {
                 translator.setCurrentDictionary(dics.get(aStrIn.substring(aStrIn.indexOf("=") + 1)));
@@ -29,25 +36,11 @@ public class RefactoredCommandLineToStatusClassWrapper {
                 translator.setTargetLang(aStrIn.substring(aStrIn.indexOf("=") + 1));
             }
             if (aStrIn.startsWith("--outFile=")) {
-                stdOut=false;
                 translator.setOutStream(new FileOutStream(aStrIn.substring(aStrIn.indexOf("=")+1)));
             }
             if (aStrIn.startsWith("--inFile=")) {
-                inlineread=false;
                 translator.setInputStream(new SimpleInputStream(translator.readContentFromFile(aStrIn.substring(aStrIn.indexOf("=")+1)).split("\n")));
             }
-        }
-        if (inlineread&&translator.currentDictionary!=null) {
-            translator.setInputStream(new SimpleInputStream(new String[] {strIn[strIn.length-1]}));
-        }
-        if (stdOut&translator.currentDictionary!=null) {
-            translator.setOutStream(new OutStream(){
-                public void output(String out) {
-                    System.out.print(out);
-                }
-                public void close() {
-                }
-            });
         }
     }
 }
